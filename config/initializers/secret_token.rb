8a85e83e0aa3cf7f7fc14deff1e44cbda1cce5b3
Jayden Morrison <jaydenmorrison97@gmail.com>
2013-12-17 17:58:31 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Superduper::Application.config.secret_key_base = 'e15a0e29a253b524533f1f42a9352ae79c3f25efe5b1a30f8f0aa457ee4244fe2cced596a7c5a99abcbde12e7a06b7e73f62ce5876a2e4a9a123d65792f5a521'
